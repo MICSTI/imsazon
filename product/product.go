03f14dffb89974bff37ad7b2dc0fea706ec3aacc
Michael Stifter <stifter.michael@gmx.net>
2018-01-06 20:20:46 +0100

    Extend product model and add first sample products

--- product/product.go
+++ product/product.go
@@ -15,23 +15,27 @@ type Product struct {
 	Id				ProductId
 	Name			string
 	Description		string
+	Category		string
 	ImageUrl		string
+	Price			float32
 	Quantity		int
 }
 
-func New(id ProductId, name string, description string, imageUrl string, quantity int) *Product {
+func New(id ProductId, name string, description string, category string, imageUrl string, price float32, quantity int) *Product {
 	return &Product{
 		Id:				id,
 		Name:			name,
 		Description:	description,
+		Category:		category,
 		ImageUrl:		imageUrl,
+		Price:			price,
 		Quantity:		quantity,
 	}
 }
 
 // Repository interface provides access to an in-memory product store
 type Repository interface {
-	// add a product to the store
+	// adds a product to the store
 	// if the product id already exists, the name and description properties are updated and the quantity added
 	// returns a new product object with the current stock status
 	Add(product *Product) (Product, error)
--- product/sample_products.go
+++ product/sample_products.go
@@ -11,5 +11,23 @@ var (
 
 // sample products
 var (
+	Lightsaber = &Product{
+		P0001,
+		"Lightsaber",
+		"The perfect lightsaber for an aspiring Jedi",
+		"Weapons",
+		"http://images.buystarwarstoys.com/products/9288/1-1/ahsoka-tano-toy-lightsaber.jpg",
+		999.99,
+		10,
+	}
 
+	MilleniumFalcon = &Product{
+		P0002,
+		"The Millenium Falcon",
+		"The fastest ship in the entire gallaxy - finished the Kessel Run in less than 12 parsecs",
+		"Mobility",
+		"http://ksassets.timeincuk.net/wp/uploads/sites/54/2017/11/Millenium-Falcon.jpg",
+		30000.00,
+		1,
+	}
 )

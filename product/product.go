6063e44b9cfcf129a614f5a77a7c2bf04add798a
Michael Stifter <stifter.michael@gmx.net>
2018-01-07 11:26:08 +0100

    Update product repository interface

--- product/product.go
+++ product/product.go
@@ -36,19 +36,19 @@ func New(id ProductId, name string, description string, category string, imageUr
 // Repository interface provides access to an in-memory product store
 type Repository interface {
 	// tries to find a product in the store by ProductId
-	Find(id ProductId) (Product, error)
+	Find(id ProductId) (*Product, error)
 
 	// returns an array of all products inside the store
-	FindAll() ([]Product, error)
+	FindAll() ([]*Product, error)
 
 	// adds a product to the store
 	// if the product id already exists, the name and description properties are updated and the quantity added
 	// returns a new product object with the current stock status
-	Add(product *Product) (Product, error)
+	Add(product *Product) (*Product, error)
 
 	// withdraws a product from the store
 	// returns a new product object with the current stock status
-	Withdraw(product *Product) (Product, error)
+	Withdraw(product *Product) (*Product, error)
 }
 
 var ErrProductUnknown = errors.New("Unknown product")

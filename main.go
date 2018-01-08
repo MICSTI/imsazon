25a9dfe19c63b63e721801016235eca1f798574c
Michael Stifter <stifter.michael@gmx.net>
2018-01-08 16:08:41 +0100

    Add comment for auth code in main.go

--- main.go
+++ main.go
@@ -146,6 +146,8 @@ func main() {
 
 func accessControl(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// TODO auth code would go here
+
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")

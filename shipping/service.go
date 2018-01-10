0a8ac7fe99c98ee5cbc8eb24f0e13b18a6532c8b
Michael Stifter <stifter.michael@gmx.net>
2018-01-10 15:41:53 +0100

    Try to execute HTTP request in shipping service

--- shipping/service.go
+++ shipping/service.go
@@ -4,11 +4,16 @@ import (
 	"errors"
 	orderModel "github.com/MICSTI/imsazon/models/order"
 	"time"
+	"net/http"
+	"encoding/json"
+	"io/ioutil"
 )
 
 // ErrInvalidArgument is returned when one or more arguments are invalid.
 var ErrInvalidArgument = errors.New("Invalid argument")
 var ErrShippingNotPossible = errors.New("Shipping is currently not possible for this order")
+var ErrInvalidOperation = errors.New("Invalid operation")
+var ErrApi = errors.New("Error response from API")
 
 // Service is the interface that provides the shipping methods
 type Service interface {
@@ -20,13 +25,25 @@ type service struct {
 
 }
 
-func (s *service) Ship(orderId orderModel.OrderId) (err error) {
+type OrderStatusApiResponse struct {
+	Order			*orderModel.Order		`json:"order"`
+}
+
+func (s *service) Ship(orderId orderModel.OrderId) (error) {
 	if orderId == "" {
 		return ErrInvalidArgument
 	}
 
 	// check the order service if the current order status is "Payment Successful"
+	currentOrderStatus, err := getOrderStatus(orderId)
 
+	if err != nil {
+		return err
+	}
+
+	if currentOrderStatus != orderModel.PaymentSuccessful {
+		return ErrInvalidOperation
+	}
 
 	// we can't really do anything, so we just add a delay and trigger the sending of an email
 	duration := time.Millisecond * 750
@@ -38,6 +55,39 @@ func (s *service) Ship(orderId orderModel.OrderId) (err error) {
 
 
 	return nil
+}
+
+func parseOrderStatusResponse(body []byte) (*OrderStatusApiResponse, error) {
+	var s = new(OrderStatusApiResponse)
+	err := json.Unmarshal(body, &s)
+	if err != nil {
+		return nil, err
+	}
+	return s, err
+}
+
+func getOrderStatus(id orderModel.OrderId) (orderModel.OrderStatus, error) {
+	resp, err := http.Get("http://localhost:8605/order/single/" + id)
+
+	if err != nil {
+		return orderModel.Created, ErrApi
+	}
+
+	defer resp.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return orderModel.Created, ErrApi
+	}
+
+	parsed, err := parseOrderStatusResponse(bodyBytes)
+
+	if err != nil {
+		return orderModel.Created, err
+	}
+
+	return parsed.Order.Status, nil
 }
 
 // NewService creates a shipping service
